49021753fa718c958673532ee96deb631061022b
mbzzzzz <153828164+mbzzzzz@users.noreply.github.com>
2025-05-25 16:44:39 +0500

    Update voiceflow-chat.tsx

--- components/voiceflow-chat.tsx
+++ components/voiceflow-chat.tsx
@@ -11,27 +11,22 @@ export function VoiceflowChat() {
         dangerouslySetInnerHTML={{
           __html: `
             (function(d, t) {
-                var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
-                v.onload = function() {
-                  window.voiceflow.chat.load({
-                    verify: { projectID: '681645146675ec5c9d958b8c' },
-                    url: 'https://general-runtime.voiceflow.com',
-                    versionID: 'production',
-                    voice: {
-                      url: "https://runtime-api.voiceflow.com"
-                    },
-                    // Position the chat bubble in the right bottom corner
-                    position: {
-                      horizontal: 'right',
-                      vertical: 'bottom',
-                      offsetHorizontal: '20px',
-                      offsetVertical: '20px'
-                    }
-                  });
-                }
-                v.src = "https://cdn.voiceflow.com/widget-next/bundle.mjs"; v.type = "text/javascript"; s.parentNode.insertBefore(v, s);
+              var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
+              v.onload = function() {
+                window.voiceflow.chat.load({
+                  verify: { projectID: '682f36004833894b71e71395' },
+                  url: 'https://general-runtime.voiceflow.com',
+                  versionID: 'production',
+                  voice: {
+                    url: "https://runtime-api.voiceflow.com"
+                  }
+                });
+              };
+              v.src = "https://cdn.voiceflow.com/widget-next/bundle.mjs";
+              v.type = "text/javascript";
+              s.parentNode.insertBefore(v, s);
             })(document, 'script');
-          `,
+          `
         }}
       />
     </>
